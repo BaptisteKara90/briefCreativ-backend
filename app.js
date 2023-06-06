04b4c15bea806047b301cc7e8e310a1340d3878e
Baptiste <baptiste.kara@gmail.com>
2023-06-06 15:39:59 +0200

    ajout de middleware

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/briefs', briefsRouter);
